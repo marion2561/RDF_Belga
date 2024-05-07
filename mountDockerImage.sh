5b9e2a87337239621fac81d1a3949f67b8e10e1a
Hazhar Galeh <hazharbewran@yahoo.com>
2024-05-07 09:11:31 +0200

    Modify the bash script to handle ports separately

--- mountDockerImage.sh
+++ mountDockerImage.sh
@@ -20,6 +20,8 @@ creer_image() {
 creer_ou_recreer_conteneur() {
     nom_image=$1
     nom_conteneur=$2
+    portmap_pc=$3
+    portmap_docker=$4
 
     # Vérifier si le conteneur existe
     if [ "$(docker ps -aq -f name=$nom_conteneur)" ]; then
@@ -28,7 +30,7 @@ creer_ou_recreer_conteneur() {
     fi
 
     # Créer le conteneur à partir de l'image spécifiée
-    docker run --network rdf_network -d --name $nom_conteneur -p $3:$3 -d $nom_image
+    docker run --network rdf_network -d --name $nom_conteneur -p $portmap_pc:$portmap_docker -d $nom_image
 }
 
 docker network create rdf_network
@@ -36,18 +38,22 @@ docker network create rdf_network
 # Projet 1
 echo "Création de l'image pour le projet API_Orders"
 creer_image "api_orders" "API_Orders" "latest"
-creer_ou_recreer_conteneur "api_orders:latest" "api_orders" "85"
+creer_ou_recreer_conteneur "api_orders:latest" "api_orders" "85" "85"
 
-# Projet 1
-echo "Création de l'image pour le projet API_Gateway"
-creer_image "api_gateway" "API_Gateway" "latest"
-creer_ou_recreer_conteneur "api_gateway:latest" "api_gateway" "81"
-
-# Projet 1
+# Projet 2
 echo "Création de l'image pour le projet API_Beers"
 creer_image "api_beers" "API_Beers" "latest"
-creer_ou_recreer_conteneur "api_beers:latest" "api_beers" "2711"
+creer_ou_recreer_conteneur "api_beers:latest" "api_beers" "2711" "2711"
 
+# Projet 3
+echo "Création de l'image pour le projet API_Plats"
+creer_image "api_plats" "API_plats" "latest"
+creer_ou_recreer_conteneur "api_plats:latest" "api_plats" "8000" "80"
+
+# Projet 4
+echo "Création de l'image pour le projet API_Gateway"
+creer_image "api_gateway" "API_Gateway" "latest"
+creer_ou_recreer_conteneur "api_gateway:latest" "api_gateway" "81" "81
 
 # Créer les conteneurs
 # Vous pouvez ajouter ici des commandes pour démarrer les conteneurs avec les images fraîchement créées
